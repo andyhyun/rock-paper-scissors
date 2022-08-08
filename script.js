c2363ada1608771055360daa2f698e9f5accdd04
andyhyun <ak2572@njit.edu>
2022-08-08 18:33:52 -0400

    Add game() function

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function getComputerChoice() {
   const choice = Math.floor(Math.random() * 3);
   switch (choice) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     case 2:
-      return "Scissors";
+      return "scissors";
     default:
       return "";
   }
@@ -19,12 +19,46 @@ function playRound(playerSelection, computerSelection) {
   computerSelection = computerSelection.toLowerCase();
 
   if (playerSelection === computerSelection) {
-    return "Tie!";
+    return "tie";
   } else if(playerSelection === "rock" && computerSelection === "scissors" ||
             playerSelection === "paper" && computerSelection === "rock" ||
             playerSelection === "scissors" && computerSelection === "paper") {
-    return `You win, ${playerSelection} beats ${computerSelection}!`;
+    return "player";
   } else {
-    return `You lose, ${computerSelection} beats ${playerSelection}.`;
+    return "computer";
+  }
+}
+
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 1; i < 6; i++) {
+    let playerSelection = capitalizeFirstLetter(prompt("Rock, paper, or scissors?"));
+    let computerSelection = capitalizeFirstLetter(getComputerChoice());
+
+    let winner = playRound(playerSelection, computerSelection);
+
+    if (winner === "player") {
+      console.log(`You win round ${i}! ${playerSelection} beats ${computerSelection}`);
+      playerScore += 1;
+    } else if (winner === "computer") {
+      console.log(`You lose round ${i}! ${playerSelection} beats ${computerSelection}`);
+      computerScore += 1;
+    } else {
+      console.log(`Round ${i} is a tie!`);
+    }
+  }
+
+  if (playerScore > computerScore) {
+    console.log(`You won ${playerScore}-${computerScore}`);
+  } else if (playerScore < computerScore) {
+    console.log(`You lost ${playerScore}-${computerScore}`);
+  } else {
+    console.log("The result is a tie");
   }
 }
