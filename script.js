6e15a4a34ba0b13eb2580193373a23919fc9d33b
andyhyun <ak2572@njit.edu>
2022-08-08 16:49:31 -0400

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 "use strict";
 
-
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+    default:
+      return '';
+  }
+}
