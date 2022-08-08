7b3b61cd02284a81dc2c396c28c64bf21c3cf79d
andyhyun <ak2572@njit.edu>
2022-08-08 17:34:20 -0400

    Add playRound() function

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function getComputerChoice() {
       return "";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "Tie!";
+  } else if(playerSelection === "rock" && computerSelection === "scissors" ||
+            playerSelection === "paper" && computerSelection === "rock" ||
+            playerSelection === "scissors" && computerSelection === "paper") {
+    return `You win, ${playerSelection} beats ${computerSelection}!`;
+  } else {
+    return `You lose, ${computerSelection} beats ${playerSelection}.`;
+  }
+}
